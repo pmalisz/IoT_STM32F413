33cbf4fcb66bd0e4beff53c8d839fb384e8b6fa9
pmalisz <pmalisz@student.agh.edu.pl>
2023-01-26 21:54:04 +0100

    saving wifi

--- source/main.cpp
+++ source/main.cpp
@@ -20,19 +20,24 @@ void printNetworkInfo(){
 
 int main()
 {
-    int result = wifi.setCredentialsForAP(MBED_CONF_APP_AP_SSID, MBED_CONF_APP_AP_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    do {
-        wifi.startSoftAP();
-        result = wifi.listenForConnection();
+    int result = wifi.connectToWifiIfSaved();
+    if(result != 0){
+        result = wifi.setCredentialsForAP(MBED_CONF_APP_AP_SSID, MBED_CONF_APP_AP_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+        do {
+            wifi.startSoftAP();
+            result = wifi.listenForConnection();
 
-        if (result != 0) {
-            printf("Wifi connection error: %d\n", result);
-        }
-    }while(result != 0);
+            if (result != 0) {
+                printf("Wifi connection error: %d\n", result);
+            }
+        }while(result != 0);
+
+        wifi.saveWifiSettings();
+    }
     printf("Wifi connected\n\n");
 
     printNetworkInfo();
-
+    
     SocketManager *socketManager = new SocketManager(&wifi);
     socketManager->init();
 
